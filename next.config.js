fb46733403577a1ff76f2a5007de01381854f9f6
matthew atkins <108462359+scout0773@users.noreply.github.com>
2024-11-20 16:18:23 -0500

    fix config + readding pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-import "./src/env.js";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true
+};
 
-/** @type {import("next").NextConfig} */
-const config = {};
-
-export default config;
+export default nextConfig;
